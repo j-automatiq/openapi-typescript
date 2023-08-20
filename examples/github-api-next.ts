a1262621d873dfb56cb9570279f20af25e65db1f
Drew Powers <1369770+drwpow@users.noreply.github.com>
2023-08-19 23:24:39 -0600

    Support JSONSchema $defs (#1317)
    * Support JSONSchema $defs
    
    * Add JSONSchema $defs docs
    
    * Add robots.txt
    
    * Docs update

--- examples/github-api-next.ts
+++ examples/github-api-next.ts
@@ -79583,6 +79583,8 @@ export interface components {
   pathItems: never;
 }
 
+export type $defs = Record<string, never>;
+
 export type external = Record<string, never>;
 
 export interface operations {
--- examples/github-api.ts
+++ examples/github-api.ts
@@ -81708,6 +81708,8 @@ export interface components {
   pathItems: never;
 }
 
+export type $defs = Record<string, never>;
+
 export type external = Record<string, never>;
 
 export interface operations {
